4b28051f051515158c0f3402afd8a4788edb8006
zheu <zheuzhyck@gmail.com>
2020-02-28 11:22:53 +0300

    add grid data provider level

---
+++ components/grid/components/GridDataProvider.vue
@@ -0,0 +1,12 @@
+<script>
+import items from "../data/items";
+
+export default {
+  data: () => ({
+    items
+  }),
+  render() {
+    return this.$scopedSlots.default({ items: this.items });
+  }
+};
+</script>
--- components/grid/consumer.vue
+++ components/grid/consumer.vue
@@ -1,39 +1,42 @@
 <template>
-  <Grid v-bind="{ items }" class="border border-red-300 border-solid border-radius-3">
-    <GridHeaders slot="headers" slot-scope="{ bindings, events }" v-bind="bindings" v-on="events">
-      <template slot-scope="{}">
-        <th>name</th>
-        <th>phone</th>
-        <th>mail</th>
-        <th>profession</th>
-      </template>
-    </GridHeaders>
-    <GridColumns
-      slot="columns"
-      slot-scope="{ bindings, events, item }"
-      v-bind="bindings"
-      v-on="events"
+  <GridDataProvider>
+    <Grid
+      slot-scope="{ items }"
+      v-bind="{ items }"
+      class="border border-red-300 border-solid border-radius-3"
     >
-      <td>{{ item.name }}</td>
-      <td>{{ item.phone }}</td>
-      <td>{{ item.mail }}</td>
-      <td>{{ item.profession }}</td>
-    </GridColumns>
-  </Grid>
+      <GridHeaders slot="headers" slot-scope="{ bindings, events }" v-bind="bindings" v-on="events">
+        <template slot-scope="{}">
+          <th>name</th>
+          <th>phone</th>
+          <th>mail</th>
+          <th>profession</th>
+        </template>
+      </GridHeaders>
+      <GridColumns
+        slot="columns"
+        slot-scope="{ bindings, events, item }"
+        v-bind="bindings"
+        v-on="events"
+      >
+        <td>{{ item.name }}</td>
+        <td>{{ item.phone }}</td>
+        <td>{{ item.mail }}</td>
+        <td>{{ item.profession }}</td>
+      </GridColumns>
+    </Grid>
+  </GridDataProvider>
 </template>
 
 <script>
 import Grid from "./components/Grid";
 import GridColumns from "./components/GridColumns";
 import GridHeaders from "./components/GridHeaders";
-import items from "./data/items";
+import GridDataProvider from "./components/GridDataProvider";
 
 export default {
   name: "App",
-  components: { Grid, GridColumns, GridHeaders },
-  data: () => ({
-    items
-  })
+  components: { Grid, GridColumns, GridHeaders, GridDataProvider }
 };
 </script>
 
