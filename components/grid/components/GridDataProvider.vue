4ea642494815921917b5c6831d62a1855940a80f
zheu <zheuzhyck@gmail.com>
2020-03-16 16:28:34 +0300

    working copy

--- components/grid/components/GridDataProvider.vue
+++ components/grid/components/GridDataProvider.vue
@@ -3,10 +3,72 @@ import items from "../data/items";
 
 export default {
   data: () => ({
-    items
+    items,
+    searchBy: {},
+    sortBy: "",
+    sortDirection: "asc"
   }),
+  methods: {
+    sort(name) {
+      if (this.sortBy !== name) {
+        this.sortBy = name;
+        this.sortDirection = "asc";
+        return;
+      }
+
+      if (this.sortDirection === "asc") {
+        this.sortDirection = "desc";
+        return;
+      }
+
+      this.sortDirection = "";
+      this.sortBy = "";
+    },
+
+    search(name, value) {
+      this.$set(this.searchBy, name, value);
+    }
+  },
+
+  watch: {
+    searchBy: {
+      deep: true,
+      immediate: true,
+      handler: function(searchBy) {
+        this.items = items.filter(item =>
+          Object.entries(searchBy).every(([key, value]) =>
+            item[key].toLowerCase().includes(value.toLowerCase())
+          )
+        );
+      }
+    },
+    sortBy: {
+      deep: true,
+      immediate: true,
+      handler: function(sortBy) {
+        // const sortDirection = this.sortDirection === 'asc' ? 1 : -1
+        // this.items = sortBy !== "" ? items.sort((a, b) =>
+        //   (a[sortBy] - b[sortBy]) * sortDirection
+        // ) : items;
+      }
+    },
+    sortDirection: {
+      deep: true,
+      immediate: true,
+      handler: function(sortDirection, prev) {
+        console.error("sortDirection", sortDirection);
+      }
+    }
+  },
+
   render() {
-    return this.$scopedSlots.default({ items: this.items });
+    const { items, search, sort } = this;
+
+    return this.$scopedSlots.default({
+      items,
+      search,
+      sort
+    });
   }
 };
-</script>
+</script> 
--- components/grid/consumer.vue
+++ components/grid/consumer.vue
@@ -1,31 +1,74 @@
 <template>
-  <GridDataProvider>
-    <Grid
-      slot-scope="{ items }"
-      v-bind="{ items }"
-      class="border border-red-300 border-solid border-radius-3"
-    >
-      <GridHeaders slot="headers" slot-scope="{ bindings, events }" v-bind="bindings" v-on="events">
-        <template slot-scope="{}">
-          <th>name</th>
-          <th>phone</th>
-          <th>mail</th>
-          <th>profession</th>
-        </template>
-      </GridHeaders>
-      <GridColumns
-        slot="columns"
-        slot-scope="{ bindings, events, item }"
-        v-bind="bindings"
-        v-on="events"
+  <div class="w-full flex items-center justify-center">
+    <GridDataProvider>
+      <Grid
+        slot-scope="{ items, sort, search }"
+        v-bind="{ items }"
+        class="border border-red-300 border-solid border-radius-3"
       >
-        <td>{{ item.name }}</td>
-        <td>{{ item.phone }}</td>
-        <td>{{ item.mail }}</td>
-        <td>{{ item.profession }}</td>
-      </GridColumns>
-    </Grid>
-  </GridDataProvider>
+        <GridHeaders
+          slot="headers"
+          slot-scope="{ bindings, events }"
+          v-bind="bindings"
+          v-on="events"
+        >
+          <template>
+            <th>
+              <div class>
+                <div @click="() => sort('name')">name</div>
+                <input
+                  type="text"
+                  class="border"
+                  @change="$event => search('name', $event.target.value)"
+                >
+              </div>
+            </th>
+            <th>
+              <div>
+                <div @click="() => sort('phone')">phone</div>
+                <input
+                  type="text"
+                  class="border"
+                  @change="$event => search('phone', $event.target.value)"
+                >
+              </div>
+            </th>
+            <th>
+              <div>
+                <div @click="() => sort('mails')">mails</div>
+                <input
+                  type="text"
+                  class="border"
+                  @change="$event => search('mails', $event.target.value)"
+                >
+              </div>
+            </th>
+            <th>
+              <div>
+                <div @click="() => sort('profession')">profession</div>
+                <input
+                  type="text"
+                  class="border"
+                  @change="$event => search('profession', $event.target.value)"
+                >
+              </div>
+            </th>
+          </template>
+        </GridHeaders>
+        <GridColumns
+          slot="columns"
+          slot-scope="{ bindings, events, item }"
+          v-bind="bindings"
+          v-on="events"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.phone }}</td>
+          <td>{{ item.mail }}</td>
+          <td>{{ item.profession }}</td>
+        </GridColumns>
+      </Grid>
+    </GridDataProvider>
+  </div>
 </template>
 
 <script>
